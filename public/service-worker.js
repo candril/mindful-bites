10fe49127fb44b7f3aac689c54293115a31d1daf
Stefan Lüdin <stefan.luedin@digitecgalaxus.ch>
2025-04-26 20:41:24 +0200

    add pwa support

---
+++ public/service-worker.js
@@ -0,0 +1,43 @@
+// Service Worker for Mindful Bites PWA
+
+const CACHE_NAME = "mindful-bites-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/fav.svg",
+  "/apple-touch-icon.png",
+  "/apple-touch-icon-180x180.png",
+  "/pwa-64x64.png",
+  "/pwa-192x192.png",
+  "/pwa-512x512.png",
+  "/maskable-icon-512x512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    }),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name)),
+      );
+    }),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request)),
+  );
+});

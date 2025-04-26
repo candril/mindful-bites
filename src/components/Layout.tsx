7709b0e3a371c3a8e4d80a886c745921a71fcf9e
Stefan Lüdin <stefan.luedin@digitecgalaxus.ch>
2025-04-26 20:18:48 +0200

    add bottom navigation

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,13 @@
 import { FC, ReactNode } from "react";
 import { Header } from "./Header";
+import { BottomNav } from "./ButtonNav";
 
 export const Layout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-svh">
       <Header />
-      {children}
+      <main className="flex flex-col flex-1 pb-16">{children}</main>
+      <BottomNav />
     </div>
   );
 };

90674a3ee9c3e0584554687748067fa810ce5e51
Stefan Lüdin <stefan.luedin@digitecgalaxus.ch>
2025-05-18 20:40:30 +0200

    fix new entry picker being empty

--- src/pages/EditDrawer.tsx
+++ src/pages/EditDrawer.tsx
@@ -53,45 +53,49 @@ export const EditDrawer: FC<{
     onClose();
   }
 
-  if (selectedDay != null && !selectedEntry && dayEntries.length === 0) {
+  if (selectedDay == null) {
+    return null;
+  }
+
+  if (selectedEntry || (showEntryPicker && dayEntries.length)) {
     return (
-      <NewEntryDrawer
-        isOpen={selectedDay != null}
-        date={selectedDay.date}
+      <Drawer
+        open={selectedDay !== null}
         onOpenChange={(open) => !open && reset()}
-      />
+      >
+        <DrawerContent className="max-w-3xl m-auto p-4 space-y-8">
+          <DrawerHeader className="flex p-0">
+            <div className="flex flex-row">
+              <DrawerTitle className="flex-1 self-center justify-center text-3xl">
+                {definition && `New ${definition.name}`}
+              </DrawerTitle>
+              <Button
+                size="icon"
+                variant="outline"
+                onClick={reset}
+                className="shadow-none border-none"
+              >
+                <X className="size-4" />
+              </Button>
+            </div>
+
+            <DrawerDescription>
+              {selectedDay && format(selectedDay.date, "EEEE, dd MMMM yyyy")}
+            </DrawerDescription>
+          </DrawerHeader>
+
+          <div className="overflow-auto">{getContent()}</div>
+        </DrawerContent>
+      </Drawer>
     );
   }
 
   return (
-    <Drawer
-      open={selectedDay !== null}
+    <NewEntryDrawer
+      isOpen={selectedDay != null}
+      date={selectedDay.date}
       onOpenChange={(open) => !open && reset()}
-    >
-      <DrawerContent className="max-w-3xl m-auto p-4 space-y-8">
-        <DrawerHeader className="flex p-0">
-          <div className="flex flex-row">
-            <DrawerTitle className="flex-1 self-center justify-center text-3xl">
-              {definition && `New ${definition.name}`}
-            </DrawerTitle>
-            <Button
-              size="icon"
-              variant="outline"
-              onClick={reset}
-              className="shadow-none border-none"
-            >
-              <X className="size-4" />
-            </Button>
-          </div>
-
-          <DrawerDescription>
-            {selectedDay && format(selectedDay.date, "EEEE, dd MMMM yyyy")}
-          </DrawerDescription>
-        </DrawerHeader>
-
-        <div className="overflow-auto">{getContent()}</div>
-      </DrawerContent>
-    </Drawer>
+    />
   );
 
   function getContent() {

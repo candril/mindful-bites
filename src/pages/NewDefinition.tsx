f42ccbb7ccf3e2806f10e781e3c0410a269a4eaf
Stefan Lüdin <stefan.luedin@digitecgalaxus.ch>
2025-05-30 20:29:55 +0200

    add new entry definition page

---
+++ src/pages/NewDefinition.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import { Layout } from "@/components/Layout";
+
+const NewDefinitionPage: FC = () => {
+  return <Layout title="New">Hello</Layout>;
+};
+
+export default NewDefinitionPage;
